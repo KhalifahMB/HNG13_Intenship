cad69d53f745fcdcb4c069330f232da27f54c425
KhalifahMB <muhammadelkufahn27@gmail.com>
2025-10-16 22:36:24 +0100

    ready for redeployment

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py migrate
 web: gunicorn hng13.wsgi
--- hng13/settings.py
+++ hng13/settings.py
@@ -3,26 +3,20 @@ import os
 from datetime import timedelta
 import dj_database_url
 from dotenv import load_dotenv
+import dj_database_url
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG')
 
-# Make the app publicly accessible by default. For production you may want
-# to restrict this to specific hostnames and set the `ALLOWED_HOSTS` env var.
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS").split(",")
 
 
 # Application definition
@@ -38,15 +32,27 @@ INSTALLED_APPS = [
     # Third-party
     'rest_framework',
     'corsheaders',
-
+    "whitenoise",
     # Local
     'profile_app',
 ]
 
+
+CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', 'True') == 'True'
+if not CORS_ALLOW_ALL_ORIGINS:
+    CORS_ALLOWED_ORIGINS = os.getenv(
+        'CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
+
+CORS_ALLOW_HEADERS = [
+    'Accept',
+    'Authorization',
+    'Content-Type',
+]
+
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    # CORS middleware should come early, before CommonMiddleware
     'corsheaders.middleware.CorsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,22 +83,23 @@ WSGI_APPLICATION = 'hng13.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+if DEBUG == False:
+    DATABASES = {
+        'default': dj_database_url.config(
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        ),
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "hng",
+            "USER": "postgres",
+            "PASSWORD": "Khalifah@8442",
+            "HOST": "localhost",
+            "PORT": 5432,
+        }}
 
-
-# CORS setup
-# CORS setup - make CORS publicly accessible by default. If you want to
-# restrict origins, set the env var CORS_ALLOW_ALL_ORIGINS to 'False' and
-# provide a comma-separated list in CORS_ALLOWED_ORIGINS.
-CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', 'True') == 'True'
-if not CORS_ALLOW_ALL_ORIGINS:
-    CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
 
 # Static files
 STATIC_URL = '/static/'
