4791e654bde80392d94095737e5ead63986d9221
KhalifahMB <muhammadelkufahn27@gmail.com>
2025-11-02 15:56:38 +0100

    fix env

--- agent/utils.py
+++ agent/utils.py
@@ -1,7 +1,8 @@
 import os
 from google import genai
+from django.conf import settings
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key=settings.GEMINI_API_KEY)
 
 
 def ask_gemini(prompt):
